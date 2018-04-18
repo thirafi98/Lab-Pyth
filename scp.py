b00acd5bcf18d87615adc8195b076867cb0865b0
Thirafi Wian.A <thirafi.badai@gmail.com>
2018-04-18 14:31:38 +0700

    Square Color Picker

---
+++ scp.py
@@ -0,0 +1,100 @@
+
+import cv2   
+import numpy as np
+
+
+cap=cv2.VideoCapture(0)
+
+while(1):
+	_, img = cap.read()
+	    
+	#converting frame(img i.e BGR) to HSV (hue-saturation-value)
+
+	hsv=cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+
+	#definig the range of red color
+	red_lower=np.array([136,87,111],np.uint8)
+	red_upper=np.array([180,255,255],np.uint8)
+
+	#defining the Range of Blue color
+	blue_lower=np.array([99,115,150],np.uint8)
+	blue_upper=np.array([110,255,255],np.uint8)
+	
+	#defining the Range of yellow color
+	yellow_lower=np.array([22,60,200],np.uint8)
+	yellow_upper=np.array([60,255,255],np.uint8)
+
+	#defining the range of green color
+	lower_green = np.array([40,70,70])
+	upper_green = np.array([80,200,200])
+
+	#finding the range of red,blue, green and yellow color in the image
+	red=cv2.inRange(hsv, red_lower, red_upper)
+	blue=cv2.inRange(hsv,blue_lower,blue_upper)
+	yellow=cv2.inRange(hsv,yellow_lower,yellow_upper)
+	green=cv2.inRange(hsv, lower_green, upper_green)
+	
+	#Morphological transformation, Dilation  	
+	kernal = np.ones((5 ,5), "uint8")
+
+    	red=cv2.dilate(red, kernal)
+	res=cv2.bitwise_and(img, img, mask = red)
+
+	blue=cv2.dilate(blue,kernal)
+	res1=cv2.bitwise_and(img, img, mask = blue)
+
+	yellow=cv2.dilate(yellow,kernal)
+	res2=cv2.bitwise_and(img, img, mask = yellow)
+
+	green=cv2.dilate(green,kernal)    
+	res3=cv2.bitwise_and(img, img, mask = green)
+
+	#Tracking the Red Color
+	(_,contours,hierarchy)=cv2.findContours(red,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+	
+	for pic, contour in enumerate(contours):
+		area = cv2.contourArea(contour)
+		if(area>500):
+			
+			x,y,w,h = cv2.boundingRect(contour)	
+			img = cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),2)
+			cv2.putText(img,"RED color",(x,y),cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0,0,255))
+			
+	#Tracking the Blue Color
+	(_,contours,hierarchy)=cv2.findContours(blue,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+	for pic, contour in enumerate(contours):
+		area = cv2.contourArea(contour)
+		if(area>500):
+			x,y,w,h = cv2.boundingRect(contour)	
+			img = cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+			cv2.putText(img,"Blue color",(x,y),cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255,0,0))
+
+	#Tracking the yellow Color
+	(_,contours,hierarchy)=cv2.findContours(yellow,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+	for pic, contour in enumerate(contours):
+		area = cv2.contourArea(contour)
+		if(area>500):
+			x,y,w,h = cv2.boundingRect(contour)	
+			img = cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
+			cv2.putText(img,"yellow  color",(x,y),cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0,255,0))
+
+	#Tracking the green Color
+	(_,contours,hierarchy)=cv2.findContours(green,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+	for pic, contour in enumerate(contours):
+		area = cv2.contourArea(contour)
+		if(area>500):
+			x,y,w,h = cv2.boundingRect(contour)	
+			img = cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
+			cv2.putText(img,"green  color",(x,y),cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0,128,0))  
+            
+           
+    	#cv2.imshow("Redcolour",red)
+    	cv2.imshow("Color Tracking",img)
+    	#cv2.imshow("red",res) 	
+    	if cv2.waitKey(10) & 0xFF == ord('q'):
+    		cap.release()
+    		cv2.destroyAllWindows()
+    		break  
+          
+
+    
